718d2b33663067d44eab7563c0ca20a73009a55a
Nina <nihi0001@kea.dk>
2023-08-24 10:44:00 +0200

    forsøg på at ændre h1

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+// skift billede
 const pic = document.querySelector("img");
 pic.src = "https://picsum.photos/200/300?grayscale";
-Pic.alt = "hejhej";
+pic.alt = "hejhej123";
+console.log(pic);
 
-const info = document.querySelector("#info")
-info.textContent = "hellohello";
-info.innerHTML = "<h1> hello3</h1>"
+let h1 = document.createElement("h1");
+let overskrift = document.createTextNode("min overskrift til info");
+h1.appendChild(overskrift);
+let img = document.createElement("img");
+img.src="https://picsum.photos/id/237/200/300"
+info.appendChild(h1);
+info.appendChild(img);
+
+
+
+//const info = document.querySelector("#info")
+//info.textContent = "hellohello";
+//info.innerHTML = "<h1>hello3</h1>";
+//console.log(info);
+
 
 //let info = document.querySelector("#info");
 //info.innerHTML="<h1>HelloWorld</h1><img src='https://picsum.photos/200/300/?blur">""

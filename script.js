cf01b500162accb5b0c74a1811dc1a9e18bb26c8
Nina <nihi0001@kea.dk>
2023-08-24 10:25:58 +0200

    forsøgt at ændre tekst

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const pic = document.querySelector("img");
 pic.src = "https://picsum.photos/200/300?grayscale";
 Pic.alt = "hejhej";
 
+const info = document.querySelector("#info")
+info.textContent = "hellohello";
+info.innerHTML = "<h1> hello3</h1>"
+
+//let info = document.querySelector("#info");
+//info.innerHTML="<h1>HelloWorld</h1><img src='https://picsum.photos/200/300/?blur">""
+//let h1 = document.createElement("h1");
+//let overskrift = document.createTextNode("hello world")
+//h1.appendChild(overskrift);
